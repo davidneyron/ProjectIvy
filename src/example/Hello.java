edea3bb6c580fb9a2cac5dd8935358e1dbbe0db0
David <david.neyron@gmail.com>
2017-04-11 10:17:48 +0200

    Prise en compte du data.csv

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,7 +1,13 @@
 
 package example;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import org.apache.commons.lang.WordUtils;
+import com.opencsv.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Hello {
     public static void main(String[] args) {
@@ -9,6 +15,21 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+        int monmax=0;
+        try {
+        	CSVReader reader =  new CSVReader(new FileReader("data.csv"));
+        	try {
+        		List<String[]> myEntries = reader.readAll();
+        		for (String[] strings : myEntries) {
+        			System.out.println(Arrays.toString(strings));
+        		}
+        	}
+        	catch (IOException e) {
+        		System.out.println("standard message: IOE");
+        	}
+        }
+        catch(FileNotFoundException e) {
+        	System.out.println("standard message: FNFE");
+        }
     }
 }
-        
