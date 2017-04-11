bc055cbfd926a06296962a692498f2ecc790e400
David <david.neyron@gmail.com>
2017-04-11 15:19:25 +0200

    Max updated

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,12 +2,13 @@
 package example;
 
 import java.io.*;
+import org.apache.commons.lang.WordUtils;
 import java.util.*;
 import com.opencsv.*;
 
 public class Hello {
     public static void main(String[] args) {
-        String  message = "hello ivy !";
+        String message = "hello ivy !";
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
@@ -18,12 +19,11 @@ public class Hello {
         		List<String[]> myEntries = reader.readAll();
         		for (String[] s : myEntries) {
         			
-        			System.out.println(Arrays.toString(strings));
-				for(String nb : s){
-					System.out.println("Nombre lu : "+ nb);
-                			monmax = max(monmax, Integer.parseInt(nb));
-
-				}
+        			System.out.println(Arrays.toString(s));
+					for(String nb : s){
+						System.out.println("Nombre lu : "+ nb);
+                		monmax = max(monmax, Integer.parseInt(nb));
+					}
         		}
         		System.out.println("Mon max : " + monmax);
         		
@@ -40,6 +40,6 @@ public class Hello {
     public static int max(int a, int b)
     {
     	return a > b ? a : b;
-    	//return a; //le stagiaire est par la
+    	//return a;
     }
 }
